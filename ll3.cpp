1402708e18839c99a290f972eb0ed1866c7a159a
PrajwalSinhaVit <prajwal.sinha13@gmail.com>
2024-07-27 18:24:45 +0530

    deletion in ll and insertion in ll

--- ll3.cpp
+++ ll3.cpp
@@ -93,12 +93,82 @@ Node* removeele(Node* head,int val){
   }
   return head;
 }
+Node* insertHead(Node* head,int val){
+  Node* temp = new Node(val,head);
+  return temp;
+}
+Node* insertEnd(Node* head,int val){
+  if(head==NULL){
+    return new Node(val);
+  }
+  Node* temp = head;
+  while(temp->next!=NULL){ //so that i can stop at the lat element
+    temp=temp->next; //increment
+  }
+  Node* newNode = new Node(val);
+  temp->next = newNode;
+  return head;
+}
+Node* insertPosition(Node* head,int el,int k){
+  if(head==NULL){
+    if(k==1){
+      return new Node(el);
+    }
+    else{
+      return head;
+    }
+  }
+  if(k==1){
+     return new Node(el,head);
+  }
+  int cnt = 0;
+  Node* temp = head;
+  while(temp!=NULL){
+    cnt++;
+    if(cnt==(k-1)){
+      Node* x = new Node(el,temp->next);
+      temp->next = x;
+      break;
+    }
+    temp = temp->next;
+  }
+  return head;
+}
+Node* insertBeforeValue(Node* head,int el,int val){
+  if(head==NULL){
+    return NULL;
+  }
+  if(head->data==val){
+     return new Node(el,head);
+  }
+ 
+  Node* temp = head;
+  bool found = false;
+  while(temp->next!=NULL){//never go to last element
+   
+    if(temp->next->data==val){
+      Node* x = new Node(el,temp->next);
+      temp->next = x;
+      found=1;
+      break;
+    }
+    temp = temp->next;
+  }
+  if(found==false){
+    cout<<"Not found";
+  }
+  return head;
+}
 int main(){
   vector<int> arr = {12,5,8,7};
   Node* head = convertArr2LL(arr);
   //head = removesHead(head);
   //head=removeTail(head);
   //head = removeK(head,k);
-  head= removeele(head,5);
+  //head= removeele(head,5);
+  //head = insertHead(head,100);
+  //head = insertEnd(head,100);
+  //head = insertPosition(head,100,3);
+  head = insertBeforeValue(head,100,5);
   print(head);
 }
